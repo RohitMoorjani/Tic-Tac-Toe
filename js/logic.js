d625e88cea640a1f1179d02ed08db54c41f4e85b
RohitMoorjani <roh123890@icloud.com>
2021-10-18 18:10:24 +0530

    first commit

---
+++ js/logic.js
@@ -0,0 +1,69 @@
+console.log("Welcome to Tic Tac Toe")
+let click = new Audio("media/click.mp3");
+let win = new Audio("media/win.mp3");
+let turn = "X";
+let flag = false;
+let reset= document.getElementById("reset");
+
+
+//Function to change the turn
+const changeTurn = () => {
+    if (turn === "X")
+        return "0";
+    else
+        return "X";
+}
+
+//Function to check win
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName("text"); //boxtext now has the span element of class text
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+    ]
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && boxtext[e[0]].innerHTML!=="") {
+            document.getElementsByClassName("info")[0].innerHTML = boxtext[e[0]].innerHTML + " " + " has won";
+            flag = true;
+            win.play();
+            document.querySelector(".img").getElementsByTagName("img")[0].style.width="15rem";
+        }
+    })
+}
+
+//Game Logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {        //forEach function will call the arrow function once for each ele in the array
+    let boxtext = element.querySelector(".text"); //boxtext now has the span element of class text
+    element.addEventListener('click', () => {
+        if (boxtext.innerHTML === "" ) {             //if the grid box is empty
+            boxtext.innerHTML =turn;
+            turn = changeTurn();                 
+            click.play();
+            checkWin();                         
+            if (!flag)
+                document.getElementsByClassName("info")[0].innerHTML = "Turn for " + turn;      
+        }
+        else{
+            boxtext.innerHTML = "";
+        }
+    })
+})
+
+//Reset button logic
+reset.addEventListener("click", ()=>{
+    let boxtexts= document.querySelectorAll(".text");
+    Array.from(boxtexts).forEach(element=>{
+        element.innerText="";
+    })
+    flag=false;
+    turn="X";
+    document.querySelector(".img").getElementsByTagName("img")[0].style.width="0";
+    document.getElementsByClassName("info")[0].innerText="X goes first";
+})
